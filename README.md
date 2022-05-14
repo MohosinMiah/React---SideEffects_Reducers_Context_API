65a6e70ecca64f24750bcf374631c15211a1474f
mohosin16103308 <hamza1610330816@gmail.com>
2022-05-14 18:37:20 +0600

    Context API -> Provider and Consumer

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 <p> For More Details Eample useEffects function used : <strong>Based on Dependency</strong> and <strong>Without Dependency</strong></p>
 
 https://reactjs.org/docs/hooks-effect.html
+
+## React Context API
+
+<p> We can use React Context API two way </P>
+<p>Provider and Consumer</p>
+<p>Useing useContext Hock</p>
--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -29,13 +30,20 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+
+  <AuthContext.Provider value={{
+       isLoggedIn : isLoggedIn,
+    }}>
+
+      <MainHeader  onLogout={logoutHandler} />
+
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home  />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
+    
+
   );
 }
 
